ff364afa936cb99c760bdd114fc7f3451b4735af
pkandathil <prashantgeorge36@gmail.com>
2016-10-11 22:54:59 -0700

    Create a passwordless sudo user on a server

---
+++ create_user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+adduser $1 --disabled-password
+echo "$1 ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+passwd -d $1
+sudo adduser $1 sudo
+mkdir /home/$1/.ssh
+cd /home/$1/
+touch .ssh/authorized_keys
+chown -R $1:$1 .ssh
+chmod -R 600 .ssh
+
